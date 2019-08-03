1a5a1092dfa61bede83b6364eef172b763271f02
shuttlesaifdev <saiful@shuttlebd.com>
2019-08-03 14:38:28 +0600

    Book Crud API

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const connectDB = require("./config/db");
+
+// routes
+const books = require("./routes/api/book");
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+app.get("/", (req, res) => res.send("Hello world!"));
+
+// use Routes
+app.use("/api/books", books);
+
+const port = process.env.PORT || 8082;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
